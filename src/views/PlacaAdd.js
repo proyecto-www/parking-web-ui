85f8a5a4f6a0cf4537ad93122c05ea865f888ecb
Ruzbellit <ruzbellit.romero@correounivalle.edu.co>
2022-12-05 03:06:57 -0500

    se actualiza pagina inicio

--- src/views/PlacaAdd.js
+++ src/views/PlacaAdd.js
@@ -3,51 +3,50 @@ import { Search } from "@mui/icons-material";
 import { IconButton } from '@mui/material';
 import { useState, } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import logo from './../resources/logo-completo.png';
 
 function PlacaAdd() {
-    const [placa, setPlaca] = useState('')
-    const navigate = useNavigate()
-    const handleChange = event => {
-        setPlaca(event.target.value.toUpperCase());
-        sessionStorage.setItem("placa", event.target.value);
-        console.log('value is:', placa);
-    };
+  const [placa, setPlaca] = useState('')
+  const navigate = useNavigate()
+  const handleChange = event => {
+    setPlaca(event.target.value.toUpperCase());
+    sessionStorage.setItem("placa", event.target.value);
+    console.log('value is:', placa);
+  };
 
-    const handleClick = () => {
-        navigate('info-placa')
-    }
-    return (
-/*  <header>
-            <div className='logo'>
-                    <img src='parqueadero.png'></img>
-                    <h2 className='logo-h2'>VehicleParking</h2>
-                   <button className='logo-btn'> iniciar sesion</button>
-                
-
-                </div> 
-
-            </header>
-  */      <div>
+  const handleClick = () => {
+    navigate('info-placa')
+  }
+  return (
+    <div className="bgimg">
 
 
-            <div className="placa-add">
-                <h2 className='logo-h2'>VehicleParking</h2>
-                <form className="placa-add-form">
-                    <label className="placa-add-form-label">Ingrese su placa</label>
-                    <input className="placa-add-form-input" type='text' placeholder="" size='10' name='placa' onChange={handleChange}
-                        value={placa} />
+      <div className="placa-add vertical-center">
+        <img className="logo-parking" src={logo} width={300} height={300} alt="logo completo parking"/>
+        <form className="placa-add-form">
+          {/* <input className="placa-add-form-input" type='text' placeholder="" size='10' name='placa' onChange={handleChange}
+                        value={placa} /> */}
+          <Box sx={{paddingBottom: 10}}
+            component="form"
+            noValidate
+            autoComplete="off"
+          >
+            <TextField className="placa-input" id="standard-basic" label="Ingrese su placa" variant="outlined" width={70}/>
+            <IconButton aria-label="buscar" onClick={handleClick}>
+              <Search />
+            </IconButton>
+          </Box>
 
-                    <IconButton aria-label="buscar" onClick={handleClick}>
-                        <Search />
+        </form>
+      </div>
+      <footer>
 
-                    </IconButton>
-                </form>
-            </div>
-            <footer>
-
-            </footer>
-        </div>
-    );
+      </footer>
+    </div>
+  );
 
 
 
