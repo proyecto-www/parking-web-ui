90b94c4f9fad6f83abc2ffc90f3ec05749d3898a
David Velasco <velasco.david@correounivalle.edu.co>
2022-12-05 12:50:15 -0500

    Corrige errores en el input

--- src/views/PlacaAdd.js
+++ src/views/PlacaAdd.js
@@ -12,34 +12,36 @@ function PlacaAdd() {
   const [placa, setPlaca] = useState('')
   const navigate = useNavigate()
   const handleChange = event => {
+    console.log(    window.location.href +'info-placa'    )
     setPlaca(event.target.value.toUpperCase());
     sessionStorage.setItem("placa", event.target.value);
     console.log('value is:', placa);
   };
 
-  const handleClick = () => {
+  const handleSummit = (event) => {
+    event.preventDefault();
     navigate('info-placa')
+  
   }
   return (
     <div className="bgimg">
 
 
       <div className="placa-add vertical-center">
-        <img className="logo-parking" src={logo} width={300} height={300} alt="logo completo parking"/>
-        <form className="placa-add-form">
-          {/* <input className="placa-add-form-input" type='text' placeholder="" size='10' name='placa' onChange={handleChange}
+        <img className="logo-parking" src={logo} width={300} height={300} alt="logo completo parking" />
+        {/* <input className="placa-add-form-input" type='text' placeholder="" size='10' name='placa' onChange={handleChange}
                         value={placa} /> */}
-          <Box sx={{paddingBottom: 10}}
-            component="form"
+        <form onSubmit={handleSummit}>
+          <Box sx={{ paddingBottom: 10 }}
             noValidate
             autoComplete="off"
+            type='button'
           >
-            <TextField className="placa-input" id="standard-basic" label="Ingrese su placa" variant="outlined" width={70}/>
-            <IconButton aria-label="buscar" onClick={handleClick}>
-              <Search />
+            <TextField type='input'value={placa} onChange={handleChange} className="placa-input" id="standard-basic" label="Ingrese su placa" variant="outlined" width={70} />
+            <IconButton aria-label="buscar" type='submit'>
+              <Search type='icon'/>
             </IconButton>
           </Box>
-
         </form>
       </div>
       <footer>
